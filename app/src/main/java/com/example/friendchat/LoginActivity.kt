457cd2c5f9002eab72358169614eba385e229963
Renards Skutels <skutels26@gmail.com>
2020-08-20 18:57:22 +0300

    Hello World

---
+++ app/src/main/java/com/example/friendchat/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.example.friendchat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+        fun login_button(view: View) {
+
+        }
+
+        fun signupAccount(view: View) {
+            val createUserIntent = Intent(this, CreateUserActivity::class.java)
+            startActivity(createUserIntent)
+        }
+
+}
